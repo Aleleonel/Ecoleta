cb9cc33dd45e5384f38aabb5bb6ba6859d8c8793
aleleonel <aleleonel@gmail.com>
2020-06-04 12:18:11 -0300

    contrução do layout do formulário completada

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -43,28 +43,78 @@ const CreatePoint = () => {
                             />
                         </div>
                         <div className="field">
-                            <label htmlFor="name">Nome da entidade</label>
+                            <label htmlFor="Whatsapp">Whatsapp</label>
                             <input 
                                 type="text"
-                                name= "name"
-                                id="name"
+                                name= "Whatsapp"
+                                id="Whatsapp"
                             />
-                    </div>
+                        </div>
                     </div>
                 </fieldset>
 
                 <fieldset>
                     <legend>
                         <h2>Endereço</h2>
+                        <span>Slecione o endereço no mapa</span>
                     </legend>
+
+                    <div className="field-group">
+                        
+                        <div className="field">
+                            <label htmlFor="uf">Estado (UF)</label>
+                            <select name="uf" id="uf">
+                                <option value="0">Selecione uma UF</option>
+                            </select>
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione uma cidade</option>
+                            </select>
+                        </div>
+
+                    </div>
+
                 </fieldset>
 
                 <fieldset>
                     <legend>
                         <h2>Ítens de coleta</h2>
+                        <span>Selecione um ou mais ítens a baixo</span>
                     </legend>
-                </fieldset>
 
+                    <ul className="items-grid">
+                        <li className="selected">
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                    </ul>
+                </fieldset>
+            <button type="submit">
+                Cadastrar ponto de coleta.
+            </button>
             </form>
         </div>
     )
